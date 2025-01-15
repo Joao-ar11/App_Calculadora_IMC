488fb0bd56422b2a5771ffcc0c7e033a12241261
João_A.11 <jarlpereira29@gmail.com>
2025-01-15 17:02:57 -0300

    Add componentes de interface a ResultadoAcitivity

--- app/src/main/java/com/joao_ar11/calculadoradeimc/ResultadoActivity.kt
+++ app/src/main/java/com/joao_ar11/calculadoradeimc/ResultadoActivity.kt
@@ -1,14 +1,28 @@
 package com.joao_ar11.calculadoradeimc
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ResultadoActivity : AppCompatActivity() {
+
+    private lateinit var textPeso: TextView
+    private lateinit var textAltura: TextView
+    private lateinit var textResultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+        inicializarComponentesInterface()
+
+    }
+
+    private fun inicializarComponentesInterface() {
+        textPeso = findViewById(R.id.text_peso)
+        textAltura = findViewById(R.id.text_altura)
+        textResultado = findViewById(R.id.text_resultado)
     }
 }
