bd3886cc432e5d074006bffa35ddb301e05d36b9
João_A.11 <jarlpereira29@gmail.com>
2025-01-15 15:25:04 -0300

    Adiciona arquivos necessarios

---
+++ app/src/main/java/com/joao_ar11/calculadoradeimc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.joao_ar11.calculadoradeimc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
