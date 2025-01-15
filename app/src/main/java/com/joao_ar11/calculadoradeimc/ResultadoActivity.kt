c575bdb31714c157aaac3cc393500ca1bfcaa902
João_A.11 <jarlpereira29@gmail.com>
2025-01-15 16:27:17 -0300

    Cria ResultadoActivity

---
+++ app/src/main/java/com/joao_ar11/calculadoradeimc/ResultadoActivity.kt
@@ -0,0 +1,14 @@
+package com.joao_ar11.calculadoradeimc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
